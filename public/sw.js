47b058134e95ca3868be0d4dd354d3d4cb080dcf
il-tuo-username <la-tua@email>
2026-03-11 15:52:03 +0200

    Fix SW cache v2 - forza aggiornamento

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'zeus-chat-v1';
+const CACHE_NAME = 'zeus-chat-v2';
 const STATIC_ASSETS = ['/', '/index.html', '/app.js', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
